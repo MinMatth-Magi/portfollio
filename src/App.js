f7bdea31ddebdf76ad398a143b5b610cf62fc4ca
MinMatth-Magi <desrues.matthieu@gmail.com>
2022-07-06 11:31:06 +0200

    Ajout de react-router et d'une page d'acceuil prévenant du développement en cours du site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Bienvenue sur mon site web utilisant github pages !</h2>
+        <p>Je suis Matthieu Desrues, un développeur software et web en 5ème année à EPITECH.</p>
+        <p>J'ai eu l'idée de faire ce site afin de présenter mes expériences professionelles. </p>
+        <p>
+          Malheureusement, l'idée est très récente, et je n'ai pas le temps de réaliser
+          ce projet tout de suite mais je compte avancer sur mon temps libre.
+        </p>
+        <p>
+          En attendant je vous invite à visiter mon&nbsp;
+          <a href="https://github.com/MinMatth-Magi">GitHub</a> et mon&nbsp;
+          <a href="https://www.linkedin.com/in/matthieu-desrues-907830141">LinkedIn</a> !
+        </p>
+      </main>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/portfollio" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
